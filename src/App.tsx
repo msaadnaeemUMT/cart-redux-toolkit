3ea4e064cca32ad3ca7e99c947077c080aae8b5c
msaadnaeem <110536997+msaadnaeem@users.noreply.github.com>
2022-10-24 21:57:08 +0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer';
+import { useEffect} from 'react';
+import { AppDispatch } from './store';
+import { useSelector } from './components/CustomSelector';
+import { calculateTotal,getCartItems } from './features/cart/cartSlice';
+import { useDispatch } from 'react-redux';
+import Modal from './components/Modal';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const {cartItems,isLoading}=useSelector((state)=>state.cart)
+  const {isOpen}=useSelector((state)=>state.modal)
+  const dispatch:AppDispatch=useDispatch()
+  useEffect(()=>{
+    dispatch(calculateTotal())
+  },[cartItems])
 
+  useEffect(()=>{
+    dispatch(getCartItems())
+  },[])
+  if(isLoading){
+    return(<div className='loading'>
+      <h1>Loading....</h1>
+    </div>)
+  }
+  return (
+  <main>
+    {isOpen && <Modal/>}
+    <Navbar/>
+    <CartContainer/>
+    </main>)
+}
 export default App;
